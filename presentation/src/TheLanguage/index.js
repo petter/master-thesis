53515b55ad47eafe7be2c584f045cee6ac0c1a8d
Petter Sæther Moen <pettersmoen@gmail.com>
2021-05-29 14:53:55 +0200

    Litt mer på PTS seksjon

--- presentation/src/TheLanguage/index.js
+++ presentation/src/TheLanguage/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import { Slide, Heading } from 'spectacle';
+import React from "react";
+import {
+  Slide,
+  Heading,
+  Text,
+  Appear,
+  UnorderedList,
+  ListItem,
+} from "spectacle";
 
 const TheLanguage = () => (
-    <>
-        <Slide>
-            <Heading>The language - PTS</Heading>
-        </Slide>
-    </>
+  <>
+    <Slide>
+      <Heading>The PTS programming language</Heading>
+      <Appear>
+        <Text>Need syntax for the following:</Text>
+        <UnorderedList>
+          <ListItem> Defining packages and templates</ListItem>
+          <ListItem>Instantiating templates</ListItem>
+          <ListItem>Renaming</ListItem>
+          <ListItem>Additions</ListItem>
+        </UnorderedList>
+      </Appear>
+    </Slide>
+  </>
 );
 
 export default TheLanguage;
