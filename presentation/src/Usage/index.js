676d0b3a644725a8fd641cf946d6d5400d76bdf3
Petter Sæther Moen <pettersmoen@gmail.com>
2021-06-11 14:50:09 +0200

    Fjerne pokemoneksempel

--- presentation/src/Usage/index.js
+++ presentation/src/Usage/index.js
@@ -1,80 +1,13 @@
 import React from "react";
 import { Slide, Heading, Text, FlexBox } from "spectacle";
-import Code from '../components/Code'
-import Pokemon from './Pokemon'
 
 const Usage = () => (
   <>
     <Slide>
-      <Heading>Installing PTS</Heading>
+      <Heading>Installing and using PTS</Heading>
       <Text>Demo</Text>
     </Slide>
-    <Slide>
-      <Heading>Using PTS (in a more complete implementation)</Heading>
-      <Code language="typescript">{`
-        template FetchJSON {
-          class FetchJSON extends Component {
-            componentDidMount() {
-              fetch(this.props.url)
-                .then(response => response.json())
-                .then(data => this.setState(state => ({...state, data})))
-                .catch(error => this.setState(state => ({...state, error})));
-            }
-          }
-        } 
-      `}</Code>
-    </Slide>
-    <Slide>
-      <Heading>Using PTS (in a more complete implementation)</Heading>
-      <Code language="typescript">{`
-        template StateLogger {
-          class StateLogger extends Component {
-            componentDidUpdate() {
-              console.log("State updated!", this.state);
-            }
-          }
-        }
-      `}</Code>
-    </Slide>
-    <Slide>
-      <Heading>Using PTS (in a more complete implementation)</Heading>
-      <Code language="typescript">{`
-        pack Pokemon {
-          inst FetchJSON { FetchJSON -> Pokemon };
-          inst StateLogger { StateLogger -> Pokemon };
-          addto Pokemon {
-            render() {
-              if(this.state.error) { return ... }
-
-              if(this.state.data === undefined) return 'Loading...';
-
-              const name = this.state.data.name;
-              const pokemonType = this.state.data.types;
-              const image = this.state.data.sprites.front_default;
-              return (
-                <div>
-                  <img src={image} />
-                  <h1>{name}</h1>
-
-                  <h2>Types</h2>
-                  <ul>
-                    {pokemoneTypes.map(pokemonType => (
-                      <li>{pokemonType.name}</li>
-                    ))}
-                  </ul>
-                </div>
-              );
-            }
-          }
-        }
-      `}</Code>
-    </Slide>
-    <Slide>
-      {/* <Heading>The resulting Pokémon component</Heading> */}
-      <FlexBox height="100%">
-        <Pokemon url="https://pokeapi.co/api/v2/pokemon/ditto" />
-      </FlexBox>
-    </Slide>
+    {/* <PokemonSlides / */}
   </>
 );
 
